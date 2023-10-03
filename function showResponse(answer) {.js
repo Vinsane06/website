e5b7220c9578802954106b5b448bd5b6e5fa309e
Vinsane06 <142159714+Vinsane06@users.noreply.github.com>
2023-10-03 10:56:01 +0530

    Add files via upload

---
+++ function showResponse(answer) {.js
@@ -0,0 +1,8 @@
+function showResponse(answer) {
+    document.getElementById('response').innerHTML = `Great! You chose ${answer}!`;
+}
+
+function moveOption() {
+    alert("Nope! You can't say no!");
+    // You can add more complex animations here to make it visually interesting.
+}
